d8759b2d57584c9bf4891c513948dfdfa8013178
Bavo Van Achte <bavo.van.achte@gmail.com>
2020-09-08 21:46:04 +0200

    Added @file doxygen directive to make sure doxygen treats all the files

--- embUnit/AssertImpl.c
+++ embUnit/AssertImpl.c
@@ -1,36 +1,35 @@
-/*
- * COPYRIGHT AND PERMISSION NOTICE
- * 
- * Copyright (c) 2003 Embedded Unit Project
- * 
- * All rights reserved.
- * 
- * Permission is hereby granted, free of charge, to any person obtaining 
- * a copy of this software and associated documentation files (the 
- * "Software"), to deal in the Software without restriction, including 
- * without limitation the rights to use, copy, modify, merge, publish, 
- * distribute, and/or sell copies of the Software, and to permit persons 
- * to whom the Software is furnished to do so, provided that the above 
- * copyright notice(s) and this permission notice appear in all copies 
- * of the Software and that both the above copyright notice(s) and this 
- * permission notice appear in supporting documentation.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT 
- * OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
- * HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY 
- * SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER 
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF 
- * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- * 
- * Except as contained in this notice, the name of a copyright holder 
- * shall not be used in advertising or otherwise to promote the sale, 
- * use or other dealings in this Software without prior written 
- * authorization of the copyright holder.
+/** @file
  *
- * $Id$
+ * COPYRIGHT AND PERMISSION NOTICE
+ *
+ * Copyright (c) 2003 Embedded Unit Project
+ *
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies
+ * of the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+ * OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY
+ * SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Except as contained in this notice, the name of a copyright holder
+ * shall not be used in advertising or otherwise to promote the sale,
+ * use or other dealings in this Software without prior written
+ * authorization of the copyright holder.
  */
 #include "config.h"
 #include "stdImpl.h"
@@ -107,7 +106,7 @@ void assertImplementationUInt(unsigned long expected,unsigned long actual,unsign
 void assertImplementationCStr(const char *expected,const char *actual, unsigned long line, const char *file)
 {
 	char buffer[ASSERT_STRING_BUFFER_MAX];
-	#define exp_act_limit ((ASSERT_STRING_BUFFER_MAX-11-1)/2)/*	"exp'' was''" = 11 byte	*/
+	#define exp_act_limit_cstr ((ASSERT_STRING_BUFFER_MAX-11-1)/2)/*	"exp'' was''" = 11 byte	*/
 	int el;
 	int al;
 
@@ -124,18 +123,18 @@ void assertImplementationCStr(const char *expected,const char *actual, unsigned 
 		al = 4;
 		actual = "null";
 	}
-	if (el > exp_act_limit) {
-		if (al > exp_act_limit) {
-			al = exp_act_limit;
-			el = exp_act_limit;
+	if (el > exp_act_limit_cstr) {
+		if (al > exp_act_limit_cstr) {
+			al = exp_act_limit_cstr;
+			el = exp_act_limit_cstr;
 		} else {
-			int w = exp_act_limit + (exp_act_limit - al);
+			int w = exp_act_limit_cstr + (exp_act_limit_cstr - al);
 			if (el > w) {
 				el = w;
 			}
 		}
 	} else {
-		int w = exp_act_limit + (exp_act_limit - el);
+		int w = exp_act_limit_cstr + (exp_act_limit_cstr - el);
 		if (al > w) {
 			al = w;
 		}
@@ -164,7 +163,7 @@ void assertImplementationMem(const void *exp,const void *act, int size, unsigned
 	const char* actual = (char*)act;
 	char buffer[ASSERT_MEMORY_BUFFER_MAX];
 	char numbuf[4];
-	#define exp_act_limit ((ASSERT_MEMORY_BUFFER_MAX-11-1)/2)/*	"exp'' was''" = 11 byte	*/
+	#define exp_act_limit_mem ((ASSERT_MEMORY_BUFFER_MAX-11-1)/2)/*	"exp'' was''" = 11 byte	*/
 	int el;
 	int al;
 	int it;
@@ -182,18 +181,18 @@ void assertImplementationMem(const void *exp,const void *act, int size, unsigned
 		al = 4;
 		actual = "null";
 	}
-	if (el > exp_act_limit) {
-		if (al > exp_act_limit) {
-			al = exp_act_limit;
-			el = exp_act_limit;
+	if (el > exp_act_limit_mem) {
+		if (al > exp_act_limit_mem) {
+			al = exp_act_limit_mem;
+			el = exp_act_limit_mem;
 		} else {
-			int w = exp_act_limit + (exp_act_limit - al);
+			int w = exp_act_limit_mem + (exp_act_limit_mem - al);
 			if (el > w) {
 				el = w;
 			}
 		}
 	} else {
-		int w = exp_act_limit + (exp_act_limit - el);
+		int w = exp_act_limit_mem + (exp_act_limit_mem - el);
 		if (al > w) {
 			al = w;
 		}
