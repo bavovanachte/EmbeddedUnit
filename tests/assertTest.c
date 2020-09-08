d5af88c04ecb1d0cb4a969febf702f72423f1e26
Bavo Van Achte <bavo.van.achte@gmail.com>
2020-09-08 09:53:28 +0200

    Expanded tests to also verify the success cases

--- tests/assertTest.c
+++ tests/assertTest.c
@@ -1,5 +1,19 @@
 #include <embUnit/embUnit.h>
 
+// TEST_ASSERT_EQUAL_MEMORY(expected,actual,size)
+// TEST_ASSERT_EQUAL_UINT(expected,actual)
+// TEST_ASSERT_EQUAL_HEX(expected,actual)
+// TEST_ASSERT_EQUAL_PTR(expected,actual)
+// TEST_ASSERT_EQUAL_INT_WITH_MARGIN(expected,actual,margin)
+// TEST_ASSERT_EQUAL_WITH_MARGIN(expected,actual,margin)
+// TEST_ASSERT_EQUAL_UINT_WITH_MARGIN(expected,actual,margin)
+// TEST_ASSERT_NOT_LOWER_THAN_INT(min, actual)
+// TEST_ASSERT_NOT_HIGHER_THAN_INT(max, actual)
+// TEST_ASSERT_RANGE_INT(min, max, actual)
+// TEST_ASSERT_NOT_LOWER_THAN_UINT(min, actual)
+// TEST_ASSERT_NOT_HIGHER_THAN_UINT(max, actual)
+// TEST_ASSERT_RANGE_UINT(min, max, actual)
+
 static void setUp(void)
 {
 }
@@ -8,7 +22,7 @@ static void tearDown(void)
 {
 }
 
-static void verify(TestCaseRef test)
+static void verify_failure(TestCaseRef test)
 {
 	TestResult result = new_TestResult(NULL);
 
@@ -19,72 +33,131 @@ static void verify(TestCaseRef test)
 	}
 }
 
-static void assert_equal_string_runTest(void)
+static void verify_success(TestCaseRef test)
+{
+	TestResult result = new_TestResult(NULL);
+
+	test->isa->run(test,&result);
+
+	if (result.failureCount > 0) {
+		TEST_FAIL("fail");
+	}
+}
+
+static void assert_equal_string_runTest_fail(void)
 {
 	TEST_ASSERT_EQUAL_STRING("123","456");
 }
 
-static void assert_equal_int_runTest(void)
+static void assert_equal_string_runTest_success(void)
+{
+	volatile char* string1 = "123";
+	volatile char* string2 = "123";
+	TEST_ASSERT_EQUAL_STRING(string1, string1);
+}
+
+static void testASSERT_EQUAL_STRING(void)
+{
+	TestCase tcase_fail = new_TestCase("assert_equal_string",NULL,NULL,assert_equal_string_runTest_fail);
+	verify_failure(&tcase_fail);
+	TestCase tcase_success = new_TestCase("assert_equal_string",NULL,NULL,assert_equal_string_runTest_success);
+	verify_success(&tcase_success);
+}
+
+static void assert_equal_int_runTest_fail(void)
 {
 	TEST_ASSERT_EQUAL_INT(123,456);
 }
 
-static void assert_null_runTest(void)
+static void assert_equal_int_runTest_success(void)
+{
+	volatile int int1 = 123;
+	volatile int int2 = 123;
+	TEST_ASSERT_EQUAL_INT(int1, int2);
+}
+
+static void testASSERT_EQUAL_INT(void)
+{
+	TestCase tcase_fail = new_TestCase("assert_equal_int",NULL,NULL,assert_equal_int_runTest_fail);
+	verify_failure(&tcase_fail);
+	TestCase tcase_success = new_TestCase("assert_equal_int",NULL,NULL,assert_equal_int_runTest_success);
+	verify_success(&tcase_success);
+}
+
+static void assert_null_runTest_fail(void)
 {
 	char *p="";
 	TEST_ASSERT_NULL(p);
 }
 
-static void assert_not_null_runTest(void)
+static void assert_null_runTest_success(void)
+{
+	char *p=NULL;
+	TEST_ASSERT_NULL(p);
+}
+
+static void testASSERT_NULL(void)
+{
+	TestCase tcase_fail = new_TestCase("assert_null",NULL,NULL,assert_null_runTest_fail);
+	verify_failure(&tcase_fail);
+	TestCase tcase_success = new_TestCase("assert_null",NULL,NULL,assert_null_runTest_success);
+	verify_success(&tcase_success);
+}
+
+static void assert_not_null_runTest_fail(void)
 {
 	char *p=NULL;
 	TEST_ASSERT_NOT_NULL(p);
 }
 
-static void assert_message_runTest(void)
+static void assert_not_null_runTest_success(void)
 {
-	TEST_ASSERT_MESSAGE(0,"0");
-}
-
-static void assert_runTest(void)
-{
-	TEST_ASSERT(0);
-}
-
-static void testASSERT_EQUAL_STRING(void)
-{
-	TestCase tcase = new_TestCase("assert_equal_string",NULL,NULL,assert_equal_string_runTest);
-	verify(&tcase);
-}
-
-static void testASSERT_EQUAL_INT(void)
-{
-	TestCase tcase = new_TestCase("assert_equal_int",NULL,NULL,assert_equal_int_runTest);
-	verify(&tcase);
-}
-
-static void testASSERT_NULL(void)
-{
-	TestCase tcase = new_TestCase("assert_null",NULL,NULL,assert_null_runTest);
-	verify(&tcase);
+	char *p="";
+	TEST_ASSERT_NOT_NULL(p);
 }
 
 static void testASSERT_NOT_NULL(void)
 {
-	TestCase tcase = new_TestCase("assert_not_null",NULL,NULL,assert_not_null_runTest);
-	verify(&tcase);
+	TestCase tcase_fail = new_TestCase("assert_not_null",NULL,NULL,assert_not_null_runTest_fail);
+	verify_failure(&tcase_fail);
+	TestCase tcase_success = new_TestCase("assert_not_null",NULL,NULL,assert_not_null_runTest_success);
+	verify_success(&tcase_success);
+}
+
+static void assert_message_runTest_fail(void)
+{
+	TEST_ASSERT_MESSAGE(0,"0");
+}
+
+static void assert_message_runTest_success(void)
+{
+	TEST_ASSERT_MESSAGE(1,"1");
 }
 
 static void testASSERT_MESSAGE(void)
 {
-	TestCase tcase = new_TestCase("assert_message",NULL,NULL,assert_message_runTest);
-	verify(&tcase);
+	TestCase tcase_fail = new_TestCase("assert_message",NULL,NULL,assert_message_runTest_fail);
+	verify_failure(&tcase_fail);
+	TestCase tcase_success = new_TestCase("assert_message",NULL,NULL,assert_message_runTest_success);
+	verify_success(&tcase_success);
+}
+
+static void assert_runTest_fail(void)
+{
+	TEST_ASSERT(0);
+}
+
+static void assert_runTest_success(void)
+{
+	TEST_ASSERT(1);
 }
 
 static void testASSERT(void)
 {
-	TestCase tcase = new_TestCase("assert",NULL,NULL,assert_runTest);
-	verify(&tcase);
+	TestCase tcase_fail = new_TestCase("assert",NULL,NULL,assert_runTest_fail);
+	verify_failure(&tcase_fail);
+	TestCase tcase_success = new_TestCase("assert",NULL,NULL,assert_runTest_success);
+	verify_success(&tcase_success);
 }
 
 TestRef assertTest_tests( TestCaller *test )
